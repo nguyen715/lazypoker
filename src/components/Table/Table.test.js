22e93236f0fe5f37bb6cbb4c1e41b60688ffebf9
Minh Nguyen <65023711+nguyen715@users.noreply.github.com>
2021-03-08 14:34:24 -0500

    First commit, created Table component and Table test file.

---
+++ src/components/Table/Table.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Table from './Table.js';
+
+describe('Table', () => {
+  test('renders Table component', () => {
+    render(<Table />);
+    screen.debug();
+  });
+});
